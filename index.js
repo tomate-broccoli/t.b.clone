ba72f8667b2065e1ac0ce4342c5d3d49d6bc6701
Your Name <you@example.com>
2019-01-12 22:00:34 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+const clone = m=>o=>JSON.parse(JSON.stringify(o), (k, v)=>m[k] ? m[k](v) : v)
+module.exports.clone = clone
+
+const filter = d=>o=>Object.prototype.keys(o).reduce((p, k)=>{
+    if(d[k]) p[k] = o[k]
+    return p
+}, {})
+module.exports.filter = filter
