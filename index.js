d6187763a4fca815447a91bfb747d02ed7b20599
Your Name <you@example.com>
2019-01-14 00:57:35 +0900

    2018.01.13

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // index.js
-const clone = m=>o=>JSON.parse(JSON.stringify(o), (k, v)=>m[k] ? m[k](v) : v)
+const clone = m=>o=>JSON.parse(
+    JSON.stringify(o)
+   ,(k, v)=>m[k] ? m[k](v) : v
+)
 module.exports.clone = clone
 
-const filter = d=>o=>Object.keys(o).reduce((p, k)=>{
-    if(d[k]) p[k] = o[k]
+const filter = m=>o=>Object.keys(o).reduce((p, k)=>{
+    if(m[k]) p[k] = m[k](o[k])
     return p
 }, {})
 module.exports.filter = filter
