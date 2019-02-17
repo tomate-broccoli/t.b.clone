eebb50a0692f1e6f38a2c9114031f4be3be4fce7
Your Name <you@example.com>
2019-02-17 15:20:34 +0900

    2019.02.17

--- index.js
+++ index.js
@@ -3,16 +3,56 @@ const clone = (m={})=>o=>JSON.parse(
     JSON.stringify(o)
    ,(k, v)=>m[k] ? m[k](v) : v
 )
-module.exports.clone = clone
 
 const filter = m=>o=>Object.keys(o).reduce((p, k)=>{
     if(m[k]) p[k] = m[k](o[k])
     return p
 }, {})
-module.exports.filter = filter
 
 const conv = m=>o=>Object.keys(o).reduce((p, k)=>{
     p[k] = m[k] ? m[k](o[k]) : o[k]
     return p
 }, {})
-module.exports.conv = conv
+
+module.exports = {
+    clone: clone
+   ,filter: filter
+   ,conv: conv
+}
+
+if(module.parent) return
+
+// sample
+const dto = {
+    id: 12345
+   ,name: 'foo bar'
+   ,birthday: new Date(2018, 12-1, 31)
+   ,sex: true
+   ,comment: null
+}
+
+const _clone = clone({
+    birthday: v=>new Date(v)
+})
+console.log('** clone:', _clone(dto))
+
+const _filter = filter ({
+    id: v=>v
+   ,name: v=>v
+   ,birthday: v=>v
+   ,sex: v=>v
+})
+console.log('** filter:', _filter(dto))
+
+const _conv = conv({
+    id: v=>("0000000000"+v).slice(-10)
+   ,birthday: dt=>{
+        const y = dt.getFullYear()
+        const m = ("00"+(dt.getMonth()+1)).slice(-2)
+        const d = ("00"+(dt.getDate())).slice(-2)
+        return `${y}/${m}/${d}`
+    }
+   ,comment: v=>v ? v : ''
+})
+console.log('** conv:', _conv(dto))
+
